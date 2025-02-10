c5a990a2832d9d9df683b02f4ca84269f7927280
ArielMn22 <arielpaixao10@gmail.com>
2025-02-10 16:30:38 -0300

    feat(header): create Header component and refactor home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,19 +1,11 @@
 import "./home.css";
-import logo from "../../assets/icons/mnemosyne-logo.svg";
+import Header from "../../components/header";
 
 const Home = () => {
     return (
         <>
-            <header className="app-header">
 
-                <img src={logo} alt="Mnemosyne Logo"></img>
-
-                <div className="links">
-                    <a href="#">HOME</a>
-                    <a href="#">ADICIONAR MEMÓRIA</a>
-                </div>
-
-            </header>
+            <Header />
 
             <main className="app-main">
 
